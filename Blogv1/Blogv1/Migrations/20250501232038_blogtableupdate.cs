c460b31dee32a9c36711ac89b46a432dadfb7c3b
veliugurlu <115639567+veliugurlu@users.noreply.github.com>
2025-05-02 14:42:13 +0300

    Aspnetmvc ile blog sayfası
    Asp net mvc ile blog sayfası

---
+++ Blogv1/Blogv1/Migrations/20250501232038_blogtableupdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogv1.Migrations
+{
+    /// <inheritdoc />
+    public partial class blogtableupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Blogs");
+        }
+    }
+}
